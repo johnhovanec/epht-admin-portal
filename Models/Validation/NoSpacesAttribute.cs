1ffbe84abaf86153e52588f0682e627100f5fc60
jhovanec <jhovanec@towson.edu>
2025-05-14 14:59:43 -0400

    Add custom data validation

---
+++ Models/Validation/NoSpacesAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace epht_admin_portal.Models.Validation;
+public class NoSpacesAttribute : ValidationAttribute
+{
+    public NoSpacesAttribute()
+    {
+        ErrorMessage = "Spaces are not allowed.";
+    }
+
+    public override bool IsValid(object value)
+    {
+        var strValue = value as string;
+        return string.IsNullOrWhiteSpace(strValue) || !strValue.Contains(' ');
+    }
+}
